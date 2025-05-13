55f2fccbd4a4db46a09f3d7ca6b4888fbbfdfd5e
Paty2006 <kouakepngounoupatricia@gmail.com>
2025-05-13 00:47:02 -0400

    rien

--- projet/projet/GestionnaireIngredients.cs
+++ projet/projet/GestionnaireIngredients.cs
@@ -10,9 +10,11 @@ namespace projet
     public static class GestionnaireIngredients
     {
         public static List<Ingredient> Ingredients { get; set; }
+        public static int[] QuantiteIngredients { get; set; }
 
         public static void ChargerFichier(string chemin)
         {
+            Ingredients = new List<Ingredient>();
             try
             {
                 string json = File.ReadAllText(chemin);
@@ -21,6 +23,15 @@ namespace projet
             catch (Exception ex)
             {
                 Console.WriteLine($"Erreur de chargement du fichier JSON : {ex.Message}");
+            }
+
+            //on crée une liste contenant la quantité actuelle de chaque ingrédient dans le restaurant
+            QuantiteIngredients = new int[Ingredients.Count];
+
+            //au départ, le restaurant posséede 20 exemplaires de tous les ingrédients
+            for(int i = 0; i <  QuantiteIngredients.Length; i++)
+            {
+                QuantiteIngredients[i] = 20;
             }
         }
     }
--- projet/projet/Restaurant.cs
+++ projet/projet/Restaurant.cs
@@ -16,7 +16,104 @@ namespace projet
         public string Nom { get; set; }
         public Statut Statut { get; set; }
         public List<Client> Clients { get; set; }
-        public Menu Menu { get; set; }
+        public List<Plat> MenuClient { get; set; }
+        public List<Plat> PlatsAchetes { get; set; }
 
+
+        /// <summary>
+        /// Cette méthode permet au gestionnaire de choisir directement les actions à mener au sein du restaurant
+        /// </summary>
+        public void AfficherMenuGestion()
+        {
+            bool veutQuitterMenu = false;
+            do
+            {
+                Console.WriteLine(" ********** Menu De Gestion **********\n\n");
+                Console.WriteLine($"Entrer le nombre correspondant à votre choix:\n\n" +
+                    $"(1) Regarder le statut du restaurant\n" +
+                    $"(2) S'occuper des différents clients\n" +
+                    $"(3) Ajuster le menu\n" +
+                    $"(4) Acheter de nouveaux plats\n" +
+                    $"(5) Commander des ingrédients\n" +
+                    $"(6) Quiter le menu\n");
+
+                int.TryParse(Console.ReadLine(), out int nombreEntre);
+
+                switch (nombreEntre)
+                {
+                    case 1:
+                        RegarderStatutRestaurant();
+                        break;
+                    case 2:
+                        SOccuperDesClients();
+                        break;
+                    case 3:
+                        AjusterMenu();
+                        break;
+                    case 4:
+                        AcheterNouveauPlat();
+                        break;
+                    case 5:
+                        CommanderIngredient();
+                        break;
+                    case 6:
+                        veutQuitterMenu = true;
+                        break;
+                    default:
+                        Console.WriteLine("Veuillez entrer un nombre valide");
+                        break;
+                }
+
+            } while (!veutQuitterMenu);
+        }
+
+        /// <summary>
+        /// affiche le statut du restaurant
+        /// </summary>
+        public void RegarderStatutRestaurant()
+        {
+            Console.WriteLine($"Le restaurant {Nom} est actuellement {Statut}");
+        }
+
+
+        public void SOccuperDesClients()
+        {
+
+        }
+
+        /// <summary>
+        /// cette méthode permettra d'ajouter ou de retirer un plat du menu présenté aux clients
+        /// </summary>
+        public void AjusterMenu()
+        {
+            //on s'assure que l'utilisateur entre un chiffre entre1 et 2
+            bool nombreValide = true;
+            int nombreEntre = 0;
+
+            do {
+                Console.WriteLine("Entrer le nombre correspondant:\n\n (1) Retirer un plat du menu des clients \n (2) Ajouter un plat au menu des clients");
+                nombreValide = int.TryParse(Console.ReadLine(), out nombreEntre);
+                if (nombreEntre < 1 || nombreEntre > 2)
+                    Console.WriteLine("Veuillez entrer un nombre valide");
+            } while (!nombreValide);
+             
+            //on retire un plat du menu client
+            if (nombreEntre == 1)
+            {
+                for(int i = 0; i < MenuClient.Count; i++)
+                {
+                    Console.WriteLine($"({i + 1})"
+                }
+            }
+        }
+        public void AcheterNouveauPlat()
+        {
+
+        }
+
+        public void CommanderIngredient()
+        {
+
+        }
     }
 }
